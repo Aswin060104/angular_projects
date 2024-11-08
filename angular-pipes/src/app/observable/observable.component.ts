d67ab0be92a676a7a37eca4bc753ac4fad7be87e
Aswin060104 <trajaaswin@gmail.com>
2024-11-08 10:00:11 +0530

    observable created from event

--- angular-pipes/src/app/observable/observable.component.ts
+++ angular-pipes/src/app/observable/observable.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import { from, Observable, of } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { from, fromEvent, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-observable',
   templateUrl: './observable.component.html',
   styleUrls: ['./observable.component.css']
 })
-export class ObservableComponent {
+export class ObservableComponent implements AfterViewInit{
+
+  @ViewChild('dataButton') dataButton : ElementRef;
 
   data : string[] = [];
+
+  buttonObs;
 
   observable = new Observable( (observer) => {
     setTimeout(() => { observer.next(1); }, 1000); 
@@ -23,14 +27,40 @@ export class ObservableComponent {
     this.fromObservable.subscribe((val : string) => {
 
       this.data.push(val);
-      console.log(val);
+      //console.log(val);
       
     }, ()=>{}, () => {
-      alert("All data are received")
+      //alert("All data are received")
     })  
   }
 
   ofObservable = of(...[1,2,3],["Aswin","Raja"]);
 
   fromObservable = from(["Aswin","Arul","Sugu","Rahul"]);
+
+  count = 0;
+
+  buttonClicked(){
+    this.buttonObs = fromEvent(this.dataButton.nativeElement,"click")
+    
+    this.buttonObs.subscribe( () => {
+      console.log("Hi");
+      this.createDiv();
+    })
+  }
+
+  ngAfterViewInit(){
+    this.buttonClicked();
+  }
+  
+  createDiv(){
+    let div = document.createElement('div');
+    div.innerText = 'Item ' + this.count++;
+
+    div.className = "container";
+    
+    document.getElementById('contentDiv').className = "container";
+
+    document.getElementById("contentDiv").appendChild(div);
+  }
 }
