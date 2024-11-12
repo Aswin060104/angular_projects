81aa72252385da6069fee7ee1413d5bcb711edf9
Aswin060104 <trajaaswin@gmail.com>
2024-11-12 19:33:53 +0530

    content bottom details updated

--- youtube/src/app/head/head.component.ts
+++ youtube/src/app/head/head.component.ts
@@ -14,18 +14,21 @@ export class HeadComponent {
     const div = document.getElementById('sidebar');
     const head = document.getElementById('head');
     const mainContent = document.getElementById('main-content-container');
+    const img = document.getElementById('content');
 
-    if(!this.sidebar && head && mainContent){
+    if(!this.sidebar && head && mainContent && img){
       div?.classList.remove('in');
       div?.classList.add('out');
       head.style.opacity = '1';
       mainContent.style.opacity = '1';
+      mainContent.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+      img.style.opacity = '1';
     }
-    else if(head && mainContent){
+    else if(head && mainContent && img){
       div?.classList.remove('out');
       div?.classList.add('in');
-      head.style.opacity ='0.1';
-      mainContent.style.opacity = '0.1';
+      mainContent.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+      img.style.opacity = '0.3';
     }
   }
 }
