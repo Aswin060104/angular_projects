d1e41aa38572fa27227bbd4c5f5a9352a8177630
Aswin060104 <trajaaswin@gmail.com>
2024-11-13 21:34:14 +0530

    Dimming effect is achieved

--- youtube/src/app/head/head.component.ts
+++ youtube/src/app/head/head.component.ts
@@ -10,27 +10,31 @@ export class HeadComponent {
   sidebar : boolean = false;
   showSidebar(){
     this.sidebar = !this.sidebar;
+    console.log(this.sidebar);
+    
 
     const div = document.getElementById('sidebar');
-    const head = document.getElementById('head');
-    const mainContent = document.getElementById('main-content-container');
-    const img = document.getElementById('content');
+    // const head = document.getElementById('head');
+    // const mainContent = document.getElementById('main-content-container');
+    // const img = document.getElementById('content');
 
-    if(!this.sidebar && head && mainContent && img){
+    if(!this.sidebar){
+    //   document.body.classList.remove('coverall');
       div?.classList.remove('in');
       div?.classList.add('out');
-      head.style.opacity = '1';
-      mainContent.style.opacity = '1';
-      mainContent.style.backgroundColor = 'rgba(255, 255, 255, 1)';
-      img.style.opacity = '1';
+    //   head.style.opacity = '1';
+    //   mainContent.style.opacity = '1';
+    //   mainContent.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+    //   img.style.opacity = '1';
     }
-    else if(head && mainContent && img){
+    else {
+    //   document.body.classList.add('coverall');
       div?.classList.remove('out');
       div?.classList.add('in');
-      mainContent.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-      img.style.opacity = '0.3';
+    //   mainContent.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+    //   img.style.opacity = '0.3';
     }
-  }
+}
 
   activeCard : number = 0;
   makeActive(val : number){
