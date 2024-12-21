86727830ea85017324c935512dc013a8a4c1b45e
Aswin060104 <trajaaswin@gmail.com>
2024-12-21 18:39:45 +0530

    routing added

--- angular-evaluation/src/app/app-routing.module.ts
+++ angular-evaluation/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BillingComponent } from './billing/billing.component';
+import { AdminComponent } from './admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : "", component : BillingComponent},
+  {path : "admin", component : AdminComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
